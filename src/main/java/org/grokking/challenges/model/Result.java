19831b52a7080d5d3b3325d3f1ca8b95b38dbd7f
Vinh Dang <vinhdangphuc@gmail.com>
2017-11-26 17:03:11 +0700

    create redis service

---
+++ src/main/java/org/grokking/challenges/model/Result.java
@@ -0,0 +1,18 @@
+package org.grokking.challenges.model;
+
+/**
+ * Created by vinhdp on 26/11/17.
+ */
+public class Result {
+
+    private String email;
+    private int score;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
