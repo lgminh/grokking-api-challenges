c46eb6563a2a4ae785395de13521312a372f8f75
Vinh Dang <vinhdangphuc@gmail.com>
2017-11-26 16:48:13 +0700

    create simple background job

--- src/main/java/org/grokking/challenges/App.java
+++ src/main/java/org/grokking/challenges/App.java
@@ -3,9 +3,11 @@ package org.grokking.challenges;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class App {
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
