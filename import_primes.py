25a4245f1b210de57e66379f2803bc7903c78d11
Minh Loc Gia <minhlg@ahamove.com>
2017-12-01 11:03:08 +0700

    edit script

--- import_primes.py
+++ import_primes.py
@@ -2,18 +2,18 @@ import redis
 
 def import_primes():
     r = redis.StrictRedis(host='localhost')
-    for i in xrange(1, 51):
+    for i in xrange(1, 3):
         with open('primes{}.txt'.format(i)) as file:
             for line in file.readlines():
-                if any(c.isalpha() for c in line) or line == '\r\r\n' :
+                if any(c.isalpha() for c in line) or line == '\r\r\n':
                     continue
-                s = line.strip('\r\r\n').split("    ")
+                s = line.strip('\r\r\n').split(" ")
                 t = []
                 for j in s:
-                    if j != '':
-                        t.append(j)
-                r.sadd("primes", t)
+                    if j == "":
+                        continue
+                    r.sadd("primes", j)
         file.close()
-    return 1
+
 if __name__ == '__main__':
     import_primes()
